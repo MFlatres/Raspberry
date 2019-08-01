74a3c6336895bc617a0b3035c10da6921e31d06f
mflatres <mathieu.flatres1@gmail.com>
2019-08-01 15:03:20 +0200

    pwm1.c: specify frequency instead of period.  Print some numbers.

--- blink/pwm1.c
+++ blink/pwm1.c
@@ -11,12 +11,20 @@ int main(void) {
   if(wiringPiSetupGpio() == -1)
     exit(1);
   
+  double frequency = 2.5;  // in Hz
+  printf("Required frequency: %f Hz\n", frequency);
+  
   pinMode(18, PWM_OUTPUT);
   pwmSetMode(PWM_MODE_MS);
   
   int divisor = 4095;        // 2 - 4095  ~ divisor, but doesn't have to be a power of 2 - scales linearly? with period
-  int period  = 4095;        // period in clock cycles; < 4096			       
+  int period = (int) (19.2e6 / (double)(divisor*frequency));
   int dutyC   = period/2;    // up time in clock cycles - for 50%, this should be period/2
+  
+  printf("Divisor: %i,  period: %i,  duty cycle: %i\n", divisor, period, dutyC);
+
+  frequency = 19.2e6 / (double)(divisor*period);
+  printf("Actual frequency: %f Hz\n", frequency);
   
   pwmSetClock(divisor);
   pwmSetRange(period);
