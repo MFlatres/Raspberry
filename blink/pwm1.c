9d6e2433bff4dd3c5e2a6324e1fda90c835e197e
mflatres <mathieu.flatres1@gmail.com>
2019-08-01 15:23:38 +0200

    pwm1.c: Get the frequency from the command line.  Compute divisor and period from it.

--- blink/pwm1.c
+++ blink/pwm1.c
@@ -4,21 +4,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <math.h>
 
-int main(void) {
+int main(int argc, char* argv[]) {
+  
   printf("Raspberry Pi wiringPi test program\n");
   
-  if(wiringPiSetupGpio() == -1)
+  if(argc < 2) {
+    printf("Please give the desired frequency as an argument\n");
     exit(1);
+  }
+  double frequency = strtod(argv[1], NULL);
   
-  double frequency = 2.5;  // in Hz
+  if(wiringPiSetupGpio() == -1) {
+    printf("wiringPiSetupGpio failed, exiting\n");
+    exit(1);
+  }
+  
+  //frequency = 2.5;  // in Hz
   printf("Required frequency: %f Hz\n", frequency);
   
   pinMode(18, PWM_OUTPUT);
   pwmSetMode(PWM_MODE_MS);
   
-  int divisor = 4095;        // 2 - 4095  ~ divisor, but doesn't have to be a power of 2 - scales linearly? with period
-  int period = (int) (19.2e6 / (double)(divisor*frequency));
+  int divisor = 4095 / sqrt(frequency);        // 2 - 4095  ~ divisor, but doesn't have to be a power of 2 - scales linearly? with period
+  int period  = (int) (19.2e6 / (double)(divisor*frequency));
   int dutyC   = period/2;    // up time in clock cycles - for 50%, this should be period/2
   
   printf("Divisor: %i,  period: %i,  duty cycle: %i\n", divisor, period, dutyC);
