4d4e422108bdfa39802926ade183aa6942d060e3
mflatres <mathieu.flatres1@gmail.com>
2019-08-07 20:01:00 +0200

    pwm1.c: add second frequency, distinguish between high and low frequency
    * pwm1.c:
      * add second frequency:
        * when second command-line argument is given, assume it's the second frequency
        * loop twice to assign two frequencies
        * doesn't seem possible with WiringPi
      * distinguish between high and low frequency
        * use hardware pwm if f>1.5Hz
        * use software pwm if f<1.5Hz
          * problem: software pwm only has two functions:
          	1. to specify the pin
    	2. to set the duty cycle
    	* can't seem to set the frequency...

--- blink/pwm1.c
+++ blink/pwm1.c
@@ -6,37 +6,92 @@
 #include <stdint.h>
 #include <math.h>
 
-int main(int argc, char* argv[]) {
-  
+int main(int argc, char* argv[] ) {
+
+
+
+  //  char stop;
   printf("Raspberry Pi wiringPi test program\n");
+
   
   if(argc < 2) {
     printf("Please give the desired frequency as an argument\n");
     exit(1);
   }
-  double frequency = strtod(argv[1], NULL);
+  double frequency1,frequency2;
+  frequency1 = strtod(argv[1], NULL);
+  frequency2 = 0;
+  if(argc == 3) frequency2 = strtod(argv[2], NULL);
+  
+  
+  double frequency[2] = {frequency1, frequency2};
+  //printf("%f %f\n", frequency[0], frequency[1]);
   
   if(wiringPiSetupGpio() == -1) {
     printf("wiringPiSetupGpio failed, exiting\n");
     exit(1);
   }
   
-  //frequency = 2.5;  // in Hz
-  printf("Required frequency: %f Hz\n", frequency);
-  
-  pinMode(18, PWM_OUTPUT);
-  pwmSetMode(PWM_MODE_MS);
-  
-  int divisor = 4095 / sqrt(frequency);        // 2 - 4095  ~ divisor, but doesn't have to be a power of 2 - scales linearly? with period
-  int period  = (int) (19.2e6 / (double)(divisor*frequency));
-  int dutyC   = period/2;    // up time in clock cycles - for 50%, this should be period/2
-  
-  printf("Divisor: %i,  period: %i,  duty cycle: %i\n", divisor, period, dutyC);
 
-  frequency = 19.2e6 / (double)(divisor*period);
-  printf("Actual frequency: %f Hz\n", frequency);
-  
-  pwmSetClock(divisor);
-  pwmSetRange(period);
-  pwmWrite(18, dutyC);
+  for(int i=0; i<argc-1; i++){
+    printf("\n");
+    
+    int pin;
+    if (i == 0) pin=18;  // PWM0
+    if (i == 1) pin=12;  // PWM1
+    
+    
+    
+    printf("Required frequency: %f Hz\n", frequency[i]);
+    printf("Loop %i, pin %i\n", i,pin);
+    pinMode(pin, PWM_OUTPUT);
+    
+    
+    
+    
+    if (frequency[i] >= 1.5){
+      pwmSetMode(PWM_MODE_MS);  // Call pwmSetMode() *after* pinMode()!
+      int divisor = 4095 / sqrt(frequency[i]);        // 2 - 4095  ~ divisor, but doesn't have to be a power of 2 - scales linearly? with period
+      if(divisor > 4095) divisor = 4095;
+      int period  = (int) (19.2e6 / (double)(divisor*frequency[i]));
+      if(period > 4095) period = 4095;
+      int dutyC   = period/2;    // up time in clock cycles - for 50%, this should be period/2
+      
+      printf("Divisor: %i,  period: %i,  duty cycle: %i\n", divisor, period, dutyC);
+      
+      frequency[i] = 19.2e6 / (double)(divisor*period);
+      printf("Actual frequency: %f Hz\n", frequency[i]);
+      
+      pwmSetClock(divisor);
+      pwmSetRange(period);
+      pwmWrite(pin, dutyC);
+      
+      //printf("For exit the code press: x\n");
+      //scanf("%c",&stop);    
+      //
+      //if(stop == 'x'){
+      //	pwmWrite(pin,0);
+      //exit(0);
+      //}
+    }  
+    
+    else {
+      double period[2] = {500 / frequency[0], 500/frequency[1]} ;
+      
+      //printf("For exit the code press: x\n");
+      
+      
+      while(1) {
+	pwmWrite(pin, 0);
+	printf("Down\n");
+	delay((int)period[i]);
+	
+	pwmWrite(pin, 4095);
+	printf("Up\n");
+	delay((int)period[i]);
+      }
+      
+    }
+  } 
 }
+
