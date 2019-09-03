04ae49899f53c05edcc9d4ce47c5f92746d8f327
mflatres <mathieu.flatres1@gmail.com>
2019-09-03 16:38:35 +0200

    pwm3: Moved software-PWM code to the function PWMsoftware()

--- blink/pwm3.c
+++ blink/pwm3.c
@@ -1,4 +1,3 @@
-//#include <wiringPi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -10,43 +9,47 @@
 // Initialisation of output: two different channels for two different motors
 
 #define PWM_CHANNEL1 0  //PWM0 on RPI
-#define PWM_CHANNEL2 1  //PWM1 on RPI
-
 #define pin1 18 
+
+#define PWM_CHANNEL2 1  //PWM1 on RPI
 #define pin2 19 
 
 
 //------------------------------------------------------------------------
 // Create a structure PWM which contains output and frequency
 
-  struct PWM{
-    int pin;
-    double frequency;
-    int channel;
-  };
+struct PWM{
+  int pin;
+  double frequency;
+  int channel;
+};
+
+
+//-------------------------------------------------------------------------
+// Call a function that manage software part
+
+void PWMsoftware(struct PWM a);
 
 
 //------------------------------------------------------------------------
 
 int main(int argc, char* argv[] ) {
 
-
-  // Test to control initialisation of <pigpio.h>  library
-  if (gpioInitialise() < 0)
-    {
-      fprintf(stderr, "pigpiod initialisation failed\n");
-    }
-
-    
   printf("Raspberry Pi pigpiod  test program\n");
   
   // Test to control user has defined at least on frequency as argument 
-  if(argc < 2) {
-  printf("Please give the desired frequency as an argument\n");
-  exit(1);
-   }
+  if(argc < 2 || argc > 3) {
+    printf("Please give one or two desired frequencies as arguments\n");
+    exit(1);
+  }
+  
+  
+  // Test to control initialisation of <pigpio.h>  library
+  if (gpioInitialise() < 0){
+      fprintf(stderr, "pigpiod initialisation failed\n");
+  }
 
-
+    
   // Initialisation of frequencies
   double frequency1,frequency2;
   frequency1 = strtod(argv[1], NULL);
@@ -54,32 +57,19 @@ int main(int argc, char* argv[] ) {
   if(argc == 3) frequency2 = strtod(argv[2], NULL);
   
   
-  // double frequency[2] = {frequency1, frequency2};
-  //printf("%f %f\n", frequency[0], frequency[1]);
-  
-
 //-------------------------------------------------------------------------
-//Initialisation of the 2 structures ant put it in a single array
+//Initialisation of the 2 structures and put it in a single array
 
   struct PWM PWM1 = {pin1, frequency1, PWM_CHANNEL1};
   struct PWM PWM2 = {pin2, frequency2, PWM_CHANNEL2};
 
-
   struct PWM PWMwork[2]={PWM1, PWM2};
+    
 
 //---------------------------------------------------------------------
 // Iteration on frequencies
   for  (int i=0; i<argc-1; i++){
-  
- 
-//---------------------------------------------------------------------    
-    // if (i == 0){
-    // PWMwork = PWM1;  
-    //};
-    //    if (i == 1){
-    // PWMwork = PWM2 ; 
-    // };
-
+    
 //---------------------------------------------------------------------
     gpioSetMode(PWMwork[i].pin, PI_OUTPUT);
     
@@ -107,38 +97,41 @@ int main(int argc, char* argv[] ) {
     }
 
 //------------------------------------------------------------    
-//Using of software for frequency < 1 Hz    
-    else {
-
-      //  int wiringPiSetup(void);
-      
-      // pinMode(pin, PWM_OUTPUT);
-      double period[2] = {500 / PWMwork[i].frequency, 500/ PWMwork[i].frequency} ;
-      
-      
-      while(1) {
-	gpioPWM(PWMwork[i].pin, 0);
-	delay((int)period[i]);
-	
-	gpioPWM(PWMwork[i].pin, 255);
-	delay((int)period[i]);
-      }
-    }
+//Using of software for frequency < 1 Hz (with the a function)
     
+    else {
+      PWMsoftware(PWMwork[i]);
+    }
   }
-//-----------------------------------------------------------      
-// Hardware part will keep working if we don't stop it handly
-
+  //-----------------------------------------------------------      
+  // Hardware part will keep working if we don't stop it handly
+  
   char stop; 
   printf("----------------------------------\n");
   printf("For exit the code press: x\n");
   scanf("%c",&stop);
-
+  
   if(stop == 'x'){
     gpioHardwarePWM(18,0,0);
     gpioHardwarePWM(19,0,0);
     exit(0);
-    
-  }
+  }    
+}
   
+
+
+//----------------------------------------------------------------
+// Function Software
+
+void PWMsoftware(struct PWM a){
+  double period = 500 / a.frequency;
+    
+  
+  while(1) {
+    gpioPWM(a.pin, 0);
+    gpioDelay((int)period);
+    
+    gpioPWM(a.pin, 255);
+    gpioDelay((int)period);
+  }
 }
